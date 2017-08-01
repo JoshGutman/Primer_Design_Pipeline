488eb3df6ec6f07f0e1ac7459e65e7a92d113893
JoshGutman <jng86@nau.edu>
2017-07-31 18:49:47 -0700

    Now uses Muscle

--- get_genomes.py
+++ get_genomes.py
@@ -5,34 +5,43 @@ from Bio import SeqIO
 
 """
 Will tblastn ever be run?
+Is Muscle needed? (Probably not)
+Is genome length of 500 a magic number? (Line 70)
 """
 
+
 # Driver
-def extract_seqs(target, path):
+def get_genomes(target, directory):
 
-    path = os.path.abspath(path)
+    directory = os.path.abspath(directory)
 
-    if not os.path.isfile("combined.seqs"):
-        combine_seqs(path)
-        subprocess.run("makeblastdb -in combined.seqs -dbtype nucl > /dev/null 2>&1", shell=True)
-
+    combine_seqs(directory)
+        
     run_blast(target, "blastn")
+    parse_blast_output(target)
 
-    return parse_blast_output(target)
+    subprocess.run("muscle -in tmp_muscle_in -out tmp_muscle_out.fasta", shell=True)
 
+    return parse_muscle_output()
+    
     
 
 # Create fasta used to make blast database
-def combine_seqs(path):
+def combine_seqs(directory):
+
+    if os.path.isfile("combined.seqs"):
+        return
     
     with open("combined.seqs", "w") as f:
 
-        for file in glob.glob(os.path.join(path, "*.fasta")):
+        for file in glob.glob(os.path.join(directory, "*.fasta")):
             f.write(">" + os.path.basename(file).replace(".fasta", "") + "\n")
             
             with open(file) as temp_file:
                 for record in SeqIO.parse(temp_file, "fasta"):
                     f.write(str(record.seq) + "\n")
+
+    subprocess.run("makeblastdb -in combined.seqs -dbtype nucl > /dev/null 2>&1", shell=True)
 
 
 
@@ -51,18 +60,30 @@ def run_blast(target, blast_type):
 # Get seqs outputted from blast
 def parse_blast_output(target):
 
-    out = []
-
-    with open("{}.blast.out".format(os.path.basename(target).replace(".fasta", "")), "rU") as f:
-        for line in f:
+    with open("{}.blast.out".format(os.path.basename(target).replace(".fasta", "")), "rU") as infile, open("tmp_muscle_in", "w") as outfile:
+        for line in infile:
             if line.startswith("#"):
                 continue
             else:
-                seq = line.split()[12]
-                if len(seq) == 500:
-                    out.append(seq)
+                fields = line.split()
+                # Is 500 a magic number?
+                if len(fields[12]) == 500:
+                    outfile.write(">" + fields[1] + "\n")
+                    outfile.write(fields[12] + "\n")
+
+
+
+def parse_muscle_output():
+
+    out = []
+
+    with open("tmp_muscle_out.fasta") as f:
+        for record in SeqIO.parse(f, "fasta"):
+            out.append(str(record.seq))
+                    
 
     return out
+
 
 
 if __name__ == "__main__":
@@ -71,4 +92,4 @@ if __name__ == "__main__":
 
     args = sys.argv
 
-    print(extract_seqs(args[1], args[2]))
+    print(get_genomes(args[1], args[2]))
