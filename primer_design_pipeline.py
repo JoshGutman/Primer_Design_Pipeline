5cdb6fbe98cbdb8a82ded84ec096d8b4b954a168
JoshGutman <jng86@nau.edu>
2017-08-03 23:21:35 -0700

    New args for get_seqs

--- primer_design_pipeline.py
+++ primer_design_pipeline.py
@@ -5,9 +5,9 @@ from get_genomes import get_genomes
 from generate_primers import generate_primers
 
 
-def primer_design_pipeline(target, directory, config_file, lower, upper):
+def primer_design_pipeline(target, directory, config_file, target_list, lower, upper):
 
-    seqs = get_seqs(config_file, target, lower, upper)
+    seqs = get_seqs(config_file, target, directory, target_list, lower, upper)
     genomes = get_genomes(target, directory)
     primers = generate_primers(seqs, genomes)
 
@@ -21,9 +21,10 @@ if __name__ == "__main__":
     parser.add_argument("-t", "--target", help="[REQUIRED] Path to target multifasta", required=True)
     parser.add_argument("-d", "--directory", help="[REQUIRED] Path to reference fastas", required=True)
     parser.add_argument("-c", "--config", help="[REQUIRED] Path to primer3 config file", required=True)
+    parser.add_argument("-g", "--genomes", help="[REQUIRED] Path to .txt file with target genomes", required=True)
     parser.add_argument("-l", "--lower", help="Lower bound of amplicon size", type=int, default=150)
     parser.add_argument("-u", "--upper", help="Upper bound of amplicon size", type=int, default=250)
 
     args = parser.parse_args()
 
-    primer_design_pipeline(args.target, args.directory, args.config, args.lower, args.upper)
+    primer_design_pipeline(args.target, args.directory, args.config, args.genomes, args.lower, args.upper)
