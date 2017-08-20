56ecd3855c246ae3eca566ec50150938d22807d9
JoshGutman <jng86@nau.edu>
2017-08-20 08:30:28 -0700

    Add divalent correction

--- get_tm.py
+++ get_tm.py
@@ -6,7 +6,13 @@ def get_tm(primer, primer_conc, na_conc, mg_conc):
     
     enthalpy, entropy = get_dH_dS(primer, primer_conc, na_conc, mg_conc)
     uncorrected_tm = get_uncorrected_tm(enthalpy, entropy, primer_conc)
-    tm = monovalent_correction(primer, na_conc, uncorrected_tm)
+
+    ratio = math.sqrt(mg_conc) / na_conc
+
+    if ratio < .22:
+        tm = monovalent_correction(primer, na_conc, uncorrected_tm)
+    else:
+        tm = divalent_correction(primer, na_conc, mg_conc, uncorrected_tm, ratio)
 
     return tm
 
@@ -85,8 +91,31 @@ def monovalent_correction(primer, na_conc, temp):
     return 1/tm_inverse - 273.15
 
 
+def divalent_correction(primer, na_conc, mg_conc, temp, ratio):
+
+    if ratio < 6:
+        a = 3.92
+        d = 1.42
+        g = 8.31
+    else:
+        a = 3.92 * (.843 - (.352 * math.sqrt(na_conc/1000.0) * math.log(na_conc/1000.0)))
+        d = 1.42 * (1.279 - (.00403 * math.log(na_conc/1000.0)) - (.00803 * math.log(na_conc/1000.0)**2.0))
+        g = 8.31 * (.486 - ((.258 * math.log(na_conc/1000.0)) + (.00525 * math.log(na_conc/1000.0)**3.0)))
+
+    gc_content = get_gc_content(primer)
+
+    term0 = 1/temp
+    term1 = a - (.91*math.log(mg_conc/1000.0))
+    term2 = gc_content * (6.26 + (d * math.log(mg_conc/1000.0)))
+    term3 = (1/(2 * (len(primer)-1))) * (-48.2 + (52.5 * math.log(mg_conc/1000.0)) + (g * math.log(mg_conc/1000.0)**2.0))
+
+    tm_inverse = term0 + ((term1 + term2 + term3) * .00001)
+
+    return 1/tm_inverse - 273.15
+
+
 
 
 
 if __name__ == "__main__":
-    print(get_tm("CTCTATCTAGCTCTCT", .25, 50, 0))
+    print(get_tm("CTCTATCTAGCTCTCT", .25, 50, 144))
