abd7823a889479a58d0794f6778359688933f54a
JoshGutman <jng86@nau.edu>
2017-08-29 03:20:47 -0700

    Changed Numpy documentation to Google documentation

--- get_tm.py
+++ get_tm.py
@@ -12,58 +12,46 @@ import math
 
 
 def get_tm(primer, oligo_conc, na_conc, mg_conc):
-    """
-    Get the melting temperature of an oligonucleotide.
+    """Get the melting temperature of an oligonucleotide.
 
     Calculate the minimum, maximum, and average melting temperature for a
     given oligonucleotide. It uses the nearest-neighbor parameters described
-    in Santalucia, 1998[1]_, the monovalent salt (Na+) correction described
-    in Owczarzy et al., 2004[2]_, and the divalent salt (Mg++) correction
-    described in Owczarzy et al., 2008[3]_.
+    in Santalucia, 1998 [1]_, the monovalent salt (Na+) correction described
+    in Owczarzy et al., 2004 [2]_, and the divalent salt (Mg++) correction
+    described in Owczarzy et al., 2008 [3]_.
 
-    Parameters
-    ----------
-    primer : str
-        A string of bases, including degeneracies.
-    oligo_conc : float
-        Olgionucleotide concentration (μM) in PCR reaction.
-    na_conc : float
-        Na+ concentration (mM) in PCR solution.
-    mg_conc : float
-        Mg++ concentration (mM) in PCR solution.
+    Args:
+        primer (str): A string of bases, including degeneracies.
+        oligo_conc (float): Olgionucleotide concentration (μM) in PCR solution.
+        na_conc (float): Na+ concentration (mM) in PCR solution.
+        mg_conc (float): Mg++ concentration (mM) in PCR solution.
 
-    Returns
-    -------
-    out : list of floats
-        List of minimum, maximum, and average melting temperatures
+    Returns:
+        :obj:`list` of :obj:`float`: List of minimum, maximum, and average
+        melting temperatures.
 
-    Notes
-    -----
-    `oligo_conc`, `na_conc`, and `mg_conc` are inputted by the user in
-    primer_design_pipeline.py.
+    Notes:
+        Driver function
 
-    Driver function
+        `oligo_conc`, `na_conc`, and `mg_conc` are inputted by the user in
+        primer_design_pipeline.py.
 
-    References
-    ----------
-    .. [1] SantaLucia, John. "A unified view of polymer, dumbbell, and
-       oligonucleotide DNA nearest-neighbor thermodynamics." Proceedings of
-       the National Academy of Sciences 95.4 (1998): 1460-1465.
+    References:
+        .. [1] SantaLucia, John. "A unified view of polymer, dumbbell, and
+               oligonucleotide DNA nearest-neighbor thermodynamics." Proceedings
+               of the National Academy of Sciences 95.4 (1998): 1460-1465.
+        .. [2] Owczarzy, Richard, et al. "Effects of sodium ions on DNA duplex
+               oligomers: improved predictions of melting temperatures."
+               Biochemistry 43.12 (2004): 3537-3554.
+        .. [3] Owczarzy, Richard, et al. "Predicting stability of DNA duplexes
+               in solutions containing magnesium and monovalent cations."
+               Biochemistry 47.19 (2008): 5336-5353.
 
-    .. [2] Owczarzy, Richard, et al. "Effects of sodium ions on DNA duplex
-       oligomers: improved predictions of melting temperatures."
-       Biochemistry 43.12 (2004): 3537-3554.
+    Examples:
+        Default values for `oligo_conc`, `na_conc`, and `mg_conc`.
 
-    .. [3] Owczarzy, Richard, et al. "Predicting stability of DNA duplexes
-       in solutions containing magnesium and monovalent cations."
-       Biochemistry 47.19 (2008): 5336-5353.
-
-    Examples
-    --------
-    Default values for `oligo_conc`, `na_conc`, and `mg_conc`.
-
-    >>> get_tm("GAAGCTTCRYTGGTCAGTTC", .25, 50, 0)
-    [51.02, 56.56, 53.79]
+        >>> get_tm("GAAGCTTCRYTGGTCAGTTC", .25, 50, 0)
+        [51.02, 56.56, 53.79]
 
     """
     out = []
@@ -108,67 +96,59 @@ def get_tm(primer, oligo_conc, na_conc, mg_conc):
 
 
 def _get_uncorrected_tm(enthalpy, entropy, oligo_conc):
-    """
-    Calculate the uncorrected temperature
+    """Calculate the uncorrected temperature
 
     Calculate the uncorrected melting temperature of a oligonucleotide
-    using nearest-neighbor parameters described in Santalucia, 1998[1]_.
+    using nearest-neighbor parameters described in Santalucia, 1998 [1]_.
 
-    Parameters
-    ----------
-    enthalpy, entropy : float
-        Enthalpy and Entropy estimations
-    oligo_conc : float
-        Olgionucleotide concentration (μM) in PCR reaction.
+    Args:
+        enthalpy (float): Enthalpy estimation
+        entropy(float): Entropy estimation
+        oligo_conc (float): Olgionucleotide concentration (μM) in PCR reaction.
 
-    Returns
-    -------
-    float
-        Uncorrected melting temperature in Kelvin.
+    Returns:
+        float: Uncorrected melting temperature in Kelvin.
 
-    Notes
-    -----
-    `enthalpy` and `entropy` are calculated in _get_dH_dS().
-    `oligo_conc` is inputted by the user in primer_design_pipeline.py.
+    Notes:
+        `enthalpy` and `entropy` are calculated in _get_dH_dS().
 
-    The formula used is:
-    .. math:: T_{M}(^o K) = \frac{\Delta H^o}{\Delta S^o  - Rln[oligo]}
-    where R is the ideal gas constant:
-    .. math:: 1.9865\;kcal\;K^{-1}\;mol^{-1}
+        `oligo_conc` is inputted by the user in primer_design_pipeline.py.
 
-    References
-    ----------
-    .. [1] SantaLucia, John. "A unified view of polymer, dumbbell, and
-       oligonucleotide DNA nearest-neighbor thermodynamics." Proceedings of
-       the National Academy of Sciences 95.4 (1998): 1460-1465.
+        The formula used is:
+
+        .. math:: T_{M}(^o K) = \\frac{\\Delta H^o}{\\Delta S^o  - Rln[oligo]}
+
+        where R is the ideal gas constant:
+
+        .. math:: 1.9865\\;kcal\\;K^{-1}\\;mol^{-1}
+
+    References:
+        .. [1] SantaLucia, John. "A unified view of polymer, dumbbell, and
+               oligonucleotide DNA nearest-neighbor thermodynamics." Proceedings
+               of the National Academy of Sciences 95.4 (1998): 1460-1465.
 
     """
     term = 1.9865 * math.log(oligo_conc/1000000.0)
 
-    return (enthalpy / (entropy + term))
+    return enthalpy / (entropy + term)
 
 
 def _get_dH_dS(primer):
-    """
-    Calculate enthalpy and entropy.
+    """Calculate enthalpy and entropy.
 
     Calculate the estimated enthalpy and entropy of an oligonucleotide
     using the nearest-neighbor parameters described in Santalucia, 1998[1]_.
 
-    Parameters
-    ----------
-    primer : str
-        A string of bases ("A", "C", "G", or "T").
+    Args:
+        primer (str): A string of bases ("A", "C", "G", or "T").
 
-    Returns
-    -------
-    enthalpy, entropy : float
+    Returns:
+        :obj:`tuple` of :obj:`float`: Tuple of the enthalpy and entropy.
 
-    References
-    -----
-    .. [1] SantaLucia, John. "A unified view of polymer, dumbbell, and
-       oligonucleotide DNA nearest-neighbor thermodynamics." Proceedings of
-       the National Academy of Sciences 95.4 (1998): 1460-1465.
+    References:
+        .. [1] SantaLucia, John. "A unified view of polymer, dumbbell, and
+               oligonucleotide DNA nearest-neighbor thermodynamics." Proceedings
+               of the National Academy of Sciences 95.4 (1998): 1460-1465.
 
     """
     enthalpy = 0
@@ -208,65 +188,52 @@ def _get_dH_dS(primer):
 
 
 def _get_gc_content(primer):
-    """
-    Get the ratio of Gs and Cs relative to the total amount of bases.
+    """Get the ratio of Gs and Cs relative to the total amount of bases.
 
-    Parameters
-    ----------
-    primer : str
-        A string of bases ("A", "C", "G", or "T")
+    Args:
+        primer (str): A string of bases ("A", "C", "G", or "T")
 
-    Returns
-    -------
-    float
+    Returns:
+        float
 
     """
     return (primer.count("G") + primer.count("C")) / len(primer)
 
 
 def _monovalent_correction(primer, na_conc, uncorrected_tm):
-    """
-    Perform monovalent salt correction.
+    """Perform monovalent salt correction.
 
     Adjust the temperature using the monovalent salt (Na+) correction
     described in Owczarzy et al., 2004[1]_.
 
-    Parameters
-    ----------
-    primer : str
-        A string of bases ("A", "C", "G", or "T").
-    na_conc : float
-        Na+ concentration (mM) in PCR solution.
-    uncorrected_tm : float
-        Temperature calculated using nearest-neighbor parameters described
-        in Santalucia, 1998[2]_.
+    Args:
+        primer (str): A string of bases ("A", "C", "G", or "T").
+        na_conc (float): Na+ concentration (mM) in PCR solution.
+        uncorrected_tm (float): Temperature calculated using nearest-neighbor
+            parameters described in Santalucia, 1998 [2]_.
 
-    Returns
-    -------
-    float
-        Corrected melting temperature in degrees Celcius
+    Returns:
+        float: Corrected melting temperature in degrees Celcius.
 
-    See Also
-    --------
-    _divalent_correction : Correction for Mg++ concentration
+    See Also:
+        _divalent_correction : Correction for Mg++ concentration.
 
-    Notes
-    -----
-    `na_conc` is inputted by the user in primer_design_pipeline.py
-    `uncorrected_tm` is calculated in _get_uncorrected_tm()
+    Notes:
+        `na_conc` is inputted by the user in primer_design_pipeline.py.
 
-    The formula is:
-    .. math:: \frac{1}{T_{M}(Na^+)} = \frac{1}{T_{M}(1M\;Na^+)} + \Big[(4.29f_{GC}\,-\,3.95)\;ln[Na^+]\;+\;0.940\,ln^2[Na^+]\Big]
+        `uncorrected_tm` is calculated in _get_uncorrected_tm().
 
-    References
-    ----------
-    .. [1] Owczarzy, Richard, et al. "Effects of sodium ions on DNA duplex
-       oligomers: improved predictions of melting temperatures."
-       Biochemistry 43.12 (2004): 3537-3554.
+        The formula is:
 
-    .. [2] SantaLucia, John. "A unified view of polymer, dumbbell, and
-       oligonucleotide DNA nearest-neighbor thermodynamics." Proceedings of
-       the National Academy of Sciences 95.4 (1998): 1460-1465.
+        .. math:: \\frac{1}{T_{M}(Na^+)} = \\frac{1}{T_{M}(1M\\;Na^+)} + \\Big[(4.29f_{GC}\\,-\\,3.95)\\;ln[Na^+]\\;+\\;0.940\\,ln^2[Na^+]\\Big]
+
+    References:
+        .. [1] Owczarzy, Richard, et al. "Effects of sodium ions on DNA duplex
+               oligomers: improved predictions of melting temperatures."
+               Biochemistry 43.12 (2004): 3537-3554.
+        .. [2] SantaLucia, John. "A unified view of polymer, dumbbell, and
+               oligonucleotide DNA nearest-neighbor thermodynamics." Proceedings
+               of the National Academy of Sciences 95.4 (1998): 1460-1465.
 
     """
     # Avoid ZeroDivisionError
@@ -285,61 +252,49 @@ def _monovalent_correction(primer, na_conc, uncorrected_tm):
 
 
 def _divalent_correction(primer, na_conc, mg_conc, uncorrected_tm, ratio):
-    """
-    Perform divalent salt correction.
+    """Perform divalent salt correction.
 
     Adjust the temperature using the divalent salt (Mg++) correction
     described in Owczarzy et al., 2008[1]_.
 
-    Parameters
-    ----------
-    primer : str
-        A string of bases ("A", "C", "G", or "T").
-    na_conc : float
-        Na+ concentration (mM) in PCR solution.
-    mg_conc : float
-        Mg++ concentration (mM) in PCR solution.
-    uncorrected_tm : float
-        Temperature calculated using nearest-neighbor parameters described
-        in Santalucia, 1998[2]_.
-    ratio : float
-        Special ratio of Mg++ ions to Na+ ions.
+    Parameters:
+        primer (str): A string of bases ("A", "C", "G", or "T").
+        na_conc (float): Na+ concentration (mM) in PCR solution.
+        mg_conc (float): Mg++ concentration (mM) in PCR solution.
+        uncorrected_tm (float): Temperature calculated using nearest-neighbor
+            parameters described in Santalucia, 1998[2]_.
+        ratio (float): Special ratio of Mg++ ions to Na+ ions.
 
-    Returns
-    -------
-    float
-        Corrected melting temperature in degrees Celcius.
+    Returns:
+        float: Corrected melting temperature in degrees Celcius.
 
-    See Also
-    --------
-    _monovalent_correction : Correction for Na+ concentration
+    See Also:
+        _monovalent_correction : Correction for Na+ concentration.
 
-    Notes
-    -----
-    `na_conc` and `mg_conc` are inputted by the user in
-    primer_design_pipeline.py.
-    `uncorrected_tm` is calculated in _get_uncorrected_tm().
+    Notes:
+        `na_conc` and `mg_conc` are inputted by the user in
+        primer_design_pipeline.py.
 
-    `ratio` uses the following formula:
-    
-    .. math:: \frac{\sqrt{[Mg^{++}]}}{[Na^+]}
+        `uncorrected_tm` is calculated in _get_uncorrected_tm().
 
-    The formula is:
-    
-    .. math:: \frac{1}{T_{M}(Mg^{++})} = \frac{1}{T_{M}(1M\;Na^+)} + \Bigg[ a - 0.911\,ln[Mg^{++} ] + f_{GC} \times \big(6.26 + d\,ln[MG^{++}]\big) + \frac{1}{2(N_{bp}-1)} \times \big[-48.2 + 52.5\,ln[Mg^{++}] + g\,ln^2[Mg^{++}]\big]\Bigg] \times 10^{-5}
+        `ratio` uses the following formula:
 
-    a, d, and g can either be constants or vary with the `na_conc` based
-    on `ratio`.
+        .. math:: \\frac{\\sqrt{[Mg^{++}]}}{[Na^+]}
 
-    References
-    ----------
-    .. [1] Owczarzy, Richard, et al. "Predicting stability of DNA duplexes
-       in solutions containing magnesium and monovalent cations."
-       Biochemistry 47.19 (2008): 5336-5353.
+        The formula is:
 
-    .. [2] SantaLucia, John. "A unified view of polymer, dumbbell, and
-       oligonucleotide DNA nearest-neighbor thermodynamics." Proceedings of
-       the National Academy of Sciences 95.4 (1998): 1460-1465.
+        .. math:: \\frac{1}{T_{M}(Mg^{++})} = \\frac{1}{T_{M}(1M\\;Na^+)} + \\Bigg[ a - 0.911\\,ln[Mg^{++} ] + f_{GC} \\times \\big(6.26 + d\\,ln[MG^{++}]\\big) + \\frac{1}{2(N_{bp}-1)} \\times \\big[-48.2 + 52.5\\,ln[Mg^{++}] + g\\,ln^2[Mg^{++}]\\big]\\Bigg] \\times 10^{-5}
+
+        a, d, and g can either be constants or vary with the `na_conc` based
+        on `ratio`.
+
+    References:
+        .. [1] Owczarzy, Richard, et al. "Predicting stability of DNA duplexes
+               in solutions containing magnesium and monovalent cations."
+               Biochemistry 47.19 (2008): 5336-5353.
+        .. [2] SantaLucia, John. "A unified view of polymer, dumbbell, and
+               oligonucleotide DNA nearest-neighbor thermodynamics." Proceedings of
+               the National Academy of Sciences 95.4 (1998): 1460-1465.
 
     """
     # Avoid ZeroDivisionError
@@ -368,75 +323,63 @@ def _divalent_correction(primer, na_conc, mg_conc, uncorrected_tm, ratio):
 
 
 def _min_primer_conversion(primer):
-    """
-    Convert primer to sequence with least energy
+    """Convert primer to sequence with least energy.
 
     Convert all degens within primer to the bases that have the least
     energy. This allows for the minimum possible temperature to be
     calculated.
 
-    Parameters
-    ----------
-    primer : str
-        A string of bases, including degeneracies
+    Args:
+        primer (str): A string of bases, including degeneracies.
 
-    Returns
-    -------
-    primer : str
-        A string of bases, with degeneracies removed
+    Returns:
+        str: A string of bases, with degeneracies converted to base with
+        minimum energy.
 
-    See Also
-    --------
-    _max_primer_conversion : Convert primer to sequence with most energy
+    See Also:
+        _max_primer_conversion : Convert primer to sequence with most energy.
 
-    Notes
-    -----
-    From least energy to most energy, the bases are A, T, G, C
+    Notes:
+        From least energy to most energy, the bases are A, T, G, C.
 
     """
-    MIN_DEGENS = {"R": "A", "Y": "T", "S": "G", "W": "A",
+    min_degens = {"R": "A", "Y": "T", "S": "G", "W": "A",
                   "K": "T", "M": "A", "B": "T", "D": "A",
                   "H": "A", "V": "A", "N": "A"}
 
-    for degen in MIN_DEGENS:
-        primer = primer.replace(degen, MIN_DEGENS[degen])
+    for degen in min_degens:
+        primer = primer.replace(degen, min_degens[degen])
 
     return primer
 
 
 def _max_primer_conversion(primer):
-    """
-    Convert primer to sequence with most energy
+    """Convert primer to sequence with most energy.
 
     Convert all degens within primer to the bases that have the most
     energy. This allows for the maximum possible temperature to be
     calculated.
 
-    Parameters
-    ----------
-    primer : str
-        A string of bases, including degeneracies
+    Args:
+        primer (str): A string of bases, including degeneracies.
 
-    Returns
-    -------
-    primer : str
-        A string of bases, with degeneracies removed
+    Returns:
+        str: A string of bases, with degeneracies converted to base with
+        maximum energy.
 
-    See Also
-    --------
-    _min_primer_conversion : Convert primer to sequence with least energy
+    See Also:
+        _max_primer_conversion : Convert primer to sequence with most energy.
 
-    Notes
-    -----
-    From most energy to least energy, the bases are C, G, T, A
+    Notes:
+        From most energy to least energy, the bases are C, G, T, A.
 
     """
-    MAX_DEGENS = {"R": "G", "Y": "C", "S": "C", "W": "T",
+    max_degens = {"R": "G", "Y": "C", "S": "C", "W": "T",
                   "K": "G", "M": "C", "B": "C", "D": "G",
                   "H": "C", "V": "C", "N": "C"}
 
-    for degen in MAX_DEGENS:
-        primer = primer.replace(degen, MAX_DEGENS[degen])
+    for degen in max_degens:
+        primer = primer.replace(degen, max_degens[degen])
 
     return primer
 
