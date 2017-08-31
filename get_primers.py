296431c1a4ee6bc91a42af29fc0ded844c2db799
JoshGutman <jng86@nau.edu>
2017-08-31 04:00:47 -0700

    Test globals

--- get_primers.py
+++ get_primers.py
@@ -40,6 +40,11 @@ def get_primers(config_file, target, directory, target_list, lower, upper):
         
 
     """
+
+    print(COMBINED_SEQS)
+    print(CONFIG_FILE)
+    print(REFERENCE_FASTA)
+    print(KEEP_FILES)
     modify_input_file(config_file, target, lower, upper)
     subprocess.run("primer3_core -output=primer3_out < config_modified.txt", shell=True)
     primers = get_primers_from_primer3("primer3_out")
