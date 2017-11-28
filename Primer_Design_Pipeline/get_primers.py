ef0f22c6e9a4e9c278f9f0fd1ace58c0a219678b
JoshGutman <jng86@nau.edu>
2017-11-28 01:57:03 -0700

    Fix primer3 config file

--- Primer_Design_Pipeline/get_primers.py
+++ Primer_Design_Pipeline/get_primers.py
@@ -79,9 +79,9 @@ def modify_input_file(config_file, target, lower, upper, temp_args):
             elif line.startswith("PRIMER_INTERNAL_SALT_DIVALENT"):
                 config_out.write("PRIMER_INTERNAL_SALT_DIVALENT={}\n".format(temp_args[2]))
             elif line.startswith("PRIMER_DNA_CONC"):
-                config_out.write("PRIMER_DNA_CONC={}".format(temp_args[0]))
+                config_out.write("PRIMER_DNA_CONC={}\n".format(temp_args[0]))
             elif line.startswith("PRIMER_INTERNAL_DNA_CONC"):
-                config_out.write("PRIMER_INTERNAL_DNA_CONC={}".format(temp_args[0]))
+                config_out.write("PRIMER_INTERNAL_DNA_CONC={}\n".format(temp_args[0]))
             elif line.startswith("PRIMER_NUM_RETURN"):
                 pass
             else:
