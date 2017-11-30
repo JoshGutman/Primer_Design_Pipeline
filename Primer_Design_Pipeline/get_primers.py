49453a67919ad11ecde687d6245f399133c52e16
JoshGutman <jng86@nau.edu>
2017-11-29 23:21:58 -0700

    Test salt concentrations in primer3 config

--- Primer_Design_Pipeline/get_primers.py
+++ Primer_Design_Pipeline/get_primers.py
@@ -70,24 +70,26 @@ def modify_input_file(config_file, target, lower, upper, temp_args):
                 config_out.write("SEQUENCE_TEMPLATE={}\n".format(sequence))
             elif line.startswith("PRIMER_PRODUCT_SIZE_RANGE"):
                 config_out.write("PRIMER_PRODUCT_SIZE_RANGE={}-{}\n".format(lower, upper))
+
             elif line.startswith("PRIMER_SALT_MONOVALENT"):
                 config_out.write("PRIMER_SALT_MONOVALENT={}\n".format(temp_args[1]))
-            elif line.startswith("PRIMER_INTERNAL_SALT_MONOVALENT"):
-                config_out.write("PRIMER_INTERNAL_SALT_MONOVALENT={}\n".format(temp_args[1]))
-            elif line.startswith("PRIMER_SALT_DIVALENT"):
-                config_out.write("PRIMER_SALT_DIVALENT={}\n".format(temp_args[2]))
-            elif line.startswith("PRIMER_INTERNAL_SALT_DIVALENT"):
-                config_out.write("PRIMER_INTERNAL_SALT_DIVALENT={}\n".format(temp_args[2]))
-            elif line.startswith("PRIMER_DNA_CONC"):
-                config_out.write("PRIMER_DNA_CONC={}\n".format(int(temp_args[0])*1000))
-            elif line.startswith("PRIMER_INTERNAL_DNA_CONC"):
-                config_out.write("PRIMER_INTERNAL_DNA_CONC={}\n".format(int(temp_args[0])*1000))
+##            elif line.startswith("PRIMER_INTERNAL_SALT_MONOVALENT"):
+##                config_out.write("PRIMER_INTERNAL_SALT_MONOVALENT={}\n".format(temp_args[1]))
+##            elif line.startswith("PRIMER_SALT_DIVALENT"):
+##                config_out.write("PRIMER_SALT_DIVALENT={}\n".format(temp_args[2]))
+##            elif line.startswith("PRIMER_INTERNAL_SALT_DIVALENT"):
+##                config_out.write("PRIMER_INTERNAL_SALT_DIVALENT={}\n".format(temp_args[2]))
+##            elif line.startswith("PRIMER_DNA_CONC"):
+##                config_out.write("PRIMER_DNA_CONC={}\n".format(int(temp_args[0])*1000))
+##            elif line.startswith("PRIMER_INTERNAL_DNA_CONC"):
+##                config_out.write("PRIMER_INTERNAL_DNA_CONC={}\n".format(int(temp_args[0])*1000))
+##            elif line.startswith("PRIMER_SALT_CORRECTIONS"):
+##                config_out.write("PRIMER_SALT_CORRECTIONS=2\n")
+##            elif line.startswith("PRIMER_TM_FORMULA"):
+##                config_out.write("PRIMER_TM_FORMULA=1\n")
+                
             elif line.startswith("PRIMER_NUM_RETURN"):
                 pass
-            elif line.startswith("PRIMER_SALT_CORRECTIONS"):
-                config_out.write("PRIMER_SALT_CORRECTIONS=2\n")
-            elif line.startswith("PRIMER_TM_FORMULA"):
-                config_out.write("PRIMER_TM_FORMULA=1\n")
             else:
                 config_out.write(line)
 
