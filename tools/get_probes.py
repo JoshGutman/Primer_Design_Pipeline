ee1249d3cb95e7c317d0c823379802ec572399d2
JoshGutman <jng86@nau.edu>
2018-04-09 09:42:53 -0600

    Revert changes

--- tools/get_probes.py
+++ tools/get_probes.py
@@ -199,7 +199,7 @@ class CandidateList:
 
     def __init__(self, combo, candidates):
         self.combo = combo
-        self.candidates = sorted(candidates, key=lambda probe: probe.score, reverse=True)
+        self.candidates = sorted(candidates, key=lambda probe: probe.score)
     
 
 
